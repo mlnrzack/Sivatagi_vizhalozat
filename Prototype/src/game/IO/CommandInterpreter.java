da14ab20d51696dd5952d9b6796e008aaba91560
Molnár Zoltán <molnar.zack@edu.bme.hu>
2023-05-15 10:06:32 +0200

    IO copy and pump bug

--- Prototype/src/game/IO/CommandInterpreter.java
+++ Prototype/src/game/IO/CommandInterpreter.java
@@ -1,16 +1,27 @@
 package game.IO;
 
-import java.util.*;
+import java.util.Scanner;
 
 /**
  * Ez a java osztály felel a konzolon bekért parancsok értelmezéséért
  * A parancsok listájáért érdemes átnézni a dokumantáció 7.1.2-es fejezetét, ahol a Bemeneti nyelvet részletezzük
  * A parancsok a Commands osztályban találhatóak, azok hívódnak meg, miután az interpreter feldolhozta a beolvasott parancsot
- *
+ * A parancsok:
+ * generate_map
+ * addPipe
+ * addPump
+ * addCistern
+ * addSource
+ * spawnMechanic
+ * spawnSaboteur
+ * playerMove
+ * playerAction
+ * playerSetIO
+ * stat
  */
 public class CommandInterpreter
 {
     Scanner sc = new Scanner(System.in);
     Commands cmd = new Commands();
-    
 }
+
--- Prototype/src/game/IO/Commands.java
+++ Prototype/src/game/IO/Commands.java
@@ -1,7 +1,16 @@
 package game.IO;
 
-public class Commands{
+import game.GameManager;
+import game.elements.*;
+import game.interfaces.ISteppable;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedMap;
+
+public class Commands
+{
 /**
  * generate_map
  * addPipe
@@ -19,35 +28,112 @@ public class Commands{
 
     public Commands()
     {
-
+    	//TODO
     }
 
-    public void GenerateMap(String[] param)
+    public void GenerateMap(String[] param) throws IOException
     {
-        if (param.length < 1 || param.length > 1)
+        if (param.length != 1)
+        {
             System.out.println("Nem adott meg megfelelő elérési útvonalat!");
+        }
+        else
+        {
+            File f = new File(param[0]);
+            if (f.exists())
+            {
+                FileReader fileReader = null;
+                try
+                {
+                    fileReader = new FileReader(f);
+                }
+                catch (FileNotFoundException e)
+                {
+                    throw new RuntimeException(e);
+                }
+                BufferedReader br = new BufferedReader(fileReader);
+                String neighbourList;
+                //Elem Azonosító:Elem_Azonosító,Elem_Azonosító,..,Elem_Azonosító
+                while ((neighbourList = br.readLine()) != null)
+                {
+                    Element temp;
+                    String firstSplit[] = neighbourList.split(" ");
+
+                    String [] secondSplit = firstSplit[1].split(":");
+                    switch (firstSplit[0])
+                    {
+                        case "Pipe":       temp = new Pipe(); ProcessParams(secondSplit[1]);
+                        case "Pump":        temp = new Pump(); ProcessParams(secondSplit[1]);
+                        case "Cistern" :    temp = new Cistern(); ProcessParams(secondSplit[1]);
+                        case "Source" :     temp = new WaterSpring(); ProcessParams(secondSplit[1]);
+                    }
+                }
+            }
+        }
+    }
+
+    void ProcessParams(String params)
+    {
+        ArrayList<Element> elementNeighbours = new ArrayList<Element>();
+        String neighbours[] = params.split(",");
+        for (String elementArray: neighbours)
+        {
+            String[] element = elementArray.split("_");
+            //Itt van az a pont, hogy mi a faszomért ilyen komplex a bemenet, de így tűnt a legegyszerűbbnek elválasztani egymástól a dolgokat
+            switch (element[0])
+            {
+                case "Pipe":
+
+                case "Pump":
+
+                case "Cistern" :
+
+                case "Source" :
+
+            }
+
+        }
     }
     
+    /**
+     * Két féle működés kell neki, vagy csak módosítom a doksiban a bemenetét
+     * Régi bemenet: addPipe <neighbour1> <neigbour2>
+     * Új bemenet: addPipe <id> <neighbour1> <neigbour2>
+     * @param param
+     */
     public void AddPipe(String[] param)
     {
         switch (param.length)
         {
             case 0:
-                break;
-            case 1: //1 || 2:
+            case 1:
+            case 2:
+                System.out.println("Incorrect parameter");
+                return;
+            case 3:
+
+                //hozzá kell még adni az ID-t
+                //GameManager.AddSteppable(new Pipe(false,0,0,0,pipeNeighbours));
                 break;
             default:
                 break;
         }
     }
 
+    /**
+     * Szintén módosul a bemenet:
+     * addPump <id> <neighbour1> <neigbour2> … <neighbourN> <input> <output>
+     * @param param
+     */
     public void AddPump(String[] param)
     {
         switch (param.length)
         {
             case 0:
+            case 1:
                 break;
-            case 1: // 1 || 2:
+            case 5:
+
                 break;
             default:
                 break;
@@ -60,7 +146,8 @@ public class Commands{
         {
             case 0:
                 break;
-            case 1: //1 || 2:
+            case 1:
+            case 2:
                 break;
             default:
                 break;
@@ -73,7 +160,8 @@ public class Commands{
         {
             case 0:
                 break;
-            case 1: //1||2
+            case 1:
+            case 2:
                 break;
             default:
                 break;
@@ -123,6 +211,7 @@ public class Commands{
                 break;
         }
     }
+    
     public void playerSetIO(String[] param)
     {
         switch (param.length)
