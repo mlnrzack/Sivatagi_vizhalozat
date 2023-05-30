19f1b357da9811b331925b2196c0ed03b53b01ca
Bálint Aracsi <balint.aracsi@lombiq.com>
2023-05-30 18:11:03 +0200

    Adding test output logging to file and comparing test output against pattern.

--- Prototype/src/tests/Test.java
+++ Prototype/src/tests/Test.java
@@ -1,7 +1,10 @@
 package tests;
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 import java.io.ByteArrayOutputStream;
@@ -31,43 +34,39 @@ public class Test {
 		InputCommands = new ArrayList<String>();
 		ExpectedOutput = new ArrayList<String>();
 		AllTests.add(this);
-		
+
 		Name = testFile.getName().substring(0, testFile.getName().lastIndexOf('.'));
-		
+
 		BufferedReader reader;
 
-		try 
-		{
+		try {
 			reader = new BufferedReader(new FileReader(testFile));
 			String line = reader.readLine();
 
-			while (line != null && !line.equals("-- Expected Output --")) 
-			{
+			while (line != null && !line.equals("-- Expected Pattern --")) {
 				InputCommands.add(line);
 
 				line = reader.readLine();
 			}
-			
-			if (line.equals("-- Expected Output --"))
+
+			if (line != null && line.equals("-- Expected Pattern --"))
 				line = reader.readLine();
-			
-			while (line != null) 
-			{
+
+			while (line != null) {
 				ExpectedOutput.add(line);
-				line = reader.readLine();				
+				line = reader.readLine();
 			}
 
 			reader.close();
-		}
-		catch (IOException e) 
-		{
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		// It has to be in the end of the ctor, as we use the values set by ctor before this.
+
+		// It has to be in the end of the ctor, as we use the values set by ctor before
+		// this.
 		GetOutput();
 	}
-	
+
 	private void GetOutput() {
 		// Credits: https://stackoverflow.com/a/8708357
 		// Create a stream to hold the output
@@ -78,38 +77,40 @@ public class Test {
 		System.setOut(ps);
 
 		Execute();
-		 
-		String output =	testOutput.toString();
-		Output = new ArrayList<String>(Arrays.asList(output.split(System.lineSeparator())));		
-		
+
+		String output = testOutput.toString();
+		Output = new ArrayList<String>(Arrays.asList(output.split(System.lineSeparator())));
+
 		System.out.flush();
 		System.setOut(old);
 	}
-	
+
 	public void Execute() {
 		InputCommands.forEach(input -> Commands.ExecuteCommand(input));
 		GameManager.Reset();
 	}
-	
-	public static void Execute(String testName) {		
+
+	public static void Execute(String testName) {
 		Test test = GetTest(testName);
 		test.ManageSuccessfulness();
 	}
-	
+
 	public static void ListAllTests() {
 		GetAllTests().forEach(test -> System.out.println(test.Name));
 	}
-	
+
 	public static Test GetTest(String testName) {
-    	for (Test test : Test.GetAllTests()) {
+		for (Test test : Test.GetAllTests()) {
 			if (test.Name.equals(testName)) {
 				return test;
-			}	
+			}
 		}
-    	return null;
+		return null;
 	}
-	
+
 	public void ManageSuccessfulness() {
+		WriteOutputToFile();		
+		
 		if (IsSuccessful()) {
 			System.out.println(Name + " teszt sikeres");
 		} else {
@@ -122,82 +123,100 @@ public class Test {
 			System.out.println("---------------------------------------------------");
 		}
 	}
-	
+
 	public boolean IsSuccessful() {
-		return Output.equals(ExpectedOutput);
+		ArrayList<String> outputCopy = (ArrayList)Output.clone();
+		int outputLineIdx = 0;
+		
+		for (String line : ExpectedOutput) {
+			boolean found = false;			
+			
+			for (int i = outputLineIdx; i<outputCopy.size(); i++) {
+				String outputLine = outputCopy.get(i);
+				if (line.equals(outputLine)) {
+					found = true;
+					outputLineIdx = i;
+					
+					break;					
+				}
+			}
+			
+			if (found == false) {
+				return false;
+			}
+		}
+		
+		return true;
 	}
-	
+
 	public static void ExecuteAllTests() {
 		// Ctor will execute tests.
 		ArrayList<Test> tests = GetAllTests();
-		
+
 		tests.forEach(test -> test.ManageSuccessfulness());
 		int successfulTestsCount = 0;
 		for (Test t : tests) {
 			if (t.IsSuccessful())
 				successfulTestsCount++;
 		}
-		
+
 		System.out.println(successfulTestsCount + "/" + tests.size() + " teszt sikeres");
 	}
-	
-	public static ArrayList<Test> GetAllTests(){
+
+	public static ArrayList<Test> GetAllTests() {
 		if (AllTests != null && !AllTests.isEmpty())
 			return AllTests;
-		
+
 		ArrayList<Test> tests = new ArrayList<Test>();
 		File folder = new File("src/tests/TestsFolder");
-		
-	    for (final File fileEntry : folder.listFiles()) {
-	    	String fileEntryName = fileEntry.getName();
-	    	String fileEntryType = fileEntryName.substring(fileEntryName.lastIndexOf('.') + 1, fileEntryName.length()).toLowerCase();
-	        if (fileEntry.isFile() && fileEntryType.equals("txt")) {
-	        	tests.add(new Test(fileEntry));
-	        }
-	    }
-	    
-	    return AllTests;
+
+		for (final File fileEntry : folder.listFiles()) {
+			String fileEntryName = fileEntry.getName();
+			String fileEntryType = fileEntryName.substring(fileEntryName.lastIndexOf('.') + 1, fileEntryName.length())
+					.toLowerCase();
+			if (fileEntry.isFile() && fileEntryType.equals("txt")) {
+				tests.add(new Test(fileEntry));
+			}
+		}
+
+		return AllTests;
 	}
-	
+
 	public static void TestMenu() {
 		Scanner in = new Scanner(System.in);
 		String input;
-		
+
 		do {
 			Test.ShowMenuText();
 			input = in.nextLine();
 			String[] inputSplit = input.split(" ");
-			switch (inputSplit[0])
-	       	{
-	       		case "runtest":
-	       		{
-	       			if (inputSplit.length < 2) {
-	       				System.out.println("Adj meg futtatandó tesztet");
-	       			} else {
-	       				Test test = Test.GetTest(inputSplit[1]);
-	       				
-		       			if (test == null) {
-		       				System.out.println("Adj meg létező futtatandó tesztet");
-		       			} else {
-		       				test.ManageSuccessfulness();
-		       			}
-	       			}
-	       			
-	                break;
-	       		}
-	       		case "runAllTests":
-	       		{
-	       			Test.ExecuteAllTests();
-	       			
-	                break;
-	       		}
-	            default:
-	                break;
-	       	}
-		}
-		while (!input.equals("exit"));
+			switch (inputSplit[0]) {
+			case "runtest": {
+				if (inputSplit.length < 2) {
+					System.out.println("Adj meg futtatandó tesztet");
+				} else {
+					Test test = Test.GetTest(inputSplit[1]);
+
+					if (test == null) {
+						System.out.println("Adj meg létező futtatandó tesztet");
+					} else {
+						test.ManageSuccessfulness();
+					}
+				}
+
+				break;
+			}
+			case "runAllTests": {
+				Test.ExecuteAllTests();
+
+				break;
+			}
+			default:
+				break;
+			}
+		} while (!input.equals("exit"));
 	}
-	
+
 	private static void ShowMenuText() {
 		System.out.println("--------------------------------");
 		System.out.println("Tesztek:");
@@ -209,5 +228,25 @@ public class Test {
 		System.out.println("\texit");
 		System.out.println("--------------------------------");
 		System.out.print("Nagyon szépen megkérlek írj be egy parancsot:");
+	}
+	
+	// Writes to src/tests/TestsFolder/Outputs/<FileName>_Output.txt
+	private void WriteOutputToFile() {
+		ArrayList<Test> tests = new ArrayList<Test>();
+		File folder = new File("src/tests/TestsFolder/Outputs");
+		
+		
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(folder + "/" + Name + "_Output.txt"));
+            
+            for (String line : Output) {
+            	writer.write(line);
+            	writer.newLine();
+            }
+            
+            writer.close();
+        } catch(Exception e) {
+        	System.out.println(e);
+        }
 	}
 }
